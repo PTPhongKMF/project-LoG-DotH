2e5e3039328d589ecf26c1cd56a4044cc9f8b692
PTPhong-PPL <PTPhongSWB@gmail.com>
2025-01-26 15:01:27 +0700

    finished movement animation
    idle, walk, run

---
+++ Assets/_LegendofGiong/Scripts/Global/AnimatorManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour {
+    Animator animator;
+
+    private int horizontal;
+    private int vertical;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+        horizontal = Animator.StringToHash("Horizontal");
+        vertical = Animator.StringToHash("Vertical");
+    }
+
+    public void UpdateAnimatorValues(float horizontalMovement, float verticalMovement) {
+        float snappedHorizontal = SnapMovementValue(0.55f, 0.5f, 1f, horizontalMovement);
+        float snappedVertical = SnapMovementValue(0.55f, 0.5f, 1f, verticalMovement);
+
+        animator.SetFloat(horizontal, snappedHorizontal, 0.1f, Time.deltaTime);
+        animator.SetFloat(vertical, snappedVertical, 0.1f, Time.deltaTime);
+    }
+
+    private float SnapMovementValue(float threshold, float snapValue1, float snapValue2, float movementValue) {
+        if (movementValue > 0 && movementValue < threshold) {
+            return snapValue1;
+        } else if (movementValue > threshold) {
+            return snapValue2;
+        } else if (movementValue < 0 && movementValue > -threshold) {
+            return -snapValue1;
+        } else if (movementValue < -threshold) {
+            return -snapValue2;
+        } else {
+            return 0f;
+        }
+    }
+}
--- Assets/_LegendofGiong/Scripts/Global/InputManager.cs
+++ Assets/_LegendofGiong/Scripts/Global/InputManager.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour {
     PlayerControls playerControls;
+    AnimatorManager animatorManager;
 
     public Vector2 movementInput;
+    private float moveAmount;
     public float verticalInput;
     public float horizontalInput;
 
     private void Awake() {
         playerControls = new PlayerControls();
+        animatorManager = GetComponent<AnimatorManager>();
     }
 
     private void OnEnable() {
@@ -28,5 +31,7 @@ public class InputManager : MonoBehaviour {
     private void HandleMovementInput() {
         verticalInput = movementInput.y;
         horizontalInput = movementInput.x;
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+        animatorManager.UpdateAnimatorValues(0, moveAmount);
     }
 }
